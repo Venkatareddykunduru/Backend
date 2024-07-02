1d81004b18e099d4cbb10dc10f0ee2ebec552e08
Venkatareddykunduru <venkatareddykunduru123@gmail.com>
2024-07-02 20:43:27 +0530

    added html code to response object

--- app.js
+++ app.js
@@ -2,7 +2,38 @@ const http = require('http');
 
 // Create the server
 const server = http.createServer((req, res) => {
-  console.log('Hi this is venkatareddy'); 
+  console.log('Hi this is venkatareddy');
+
+
+  let title = '';
+  let message = '';
+
+  if (req.url === '/home') {
+    title = 'Home';
+    message = 'Welcome home';
+  } else if (req.url === '/about') {
+    title = 'About Us';
+    message = 'Welcome to About Us page';
+  } else if (req.url === '/node') {
+    title = 'Node Js Project';
+    message = 'Welcome to my Node Js project';
+  } else {
+    title = '404 Not Found';
+    message = 'Page not found';
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+  }
+
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>${title}</title>
+    </head>
+    <body>
+      <h1>${message}</h1>
+    </body>
+    </html>
+  `);
   
 });
 
