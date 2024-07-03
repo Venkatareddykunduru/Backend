e1fa9bcd638bf918757ca5743eccbade2747836e
Venkatareddykunduru <venkatareddykunduru123@gmail.com>
2024-07-03 12:06:59 +0530

    update app.js code which displays messages of file

--- app.js
+++ app.js
@@ -1,42 +1,58 @@
 const http = require('http');
+const fs = require('fs');
 
 // Create the server
 const server = http.createServer((req, res) => {
   console.log('Hi this is venkatareddy');
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    // Read existing messages from the file
+    fs.readFile('messages.txt', (err, data) => {
+      if (err) {
+        data = ''; // If the file doesn't exist or is empty
+      }
+      // Convert buffer to string and split by newline, then filter out empty strings
+      const messages = data.toString().split('\n').filter(Boolean);
 
-
-  let title = '';
-  let message = '';
-
-  if (req.url === '/home') {
-    title = 'Home';
-    message = 'Welcome home';
-  } else if (req.url === '/about') {
-    title = 'About Us';
-    message = 'Welcome to About Us page';
-  } else if (req.url === '/node') {
-    title = 'Node Js Project';
-    message = 'Welcome to my Node Js project';
+      // Generate HTML with messages at the top of the form
+      res.write('<html>');
+      res.write('<head><title>Enter Message</title></head>');
+      res.write('<body>');
+      res.write('<h1>Messages:</h1>');
+      res.write('<ul>');
+      messages.forEach(message => {
+        res.write(`<li>${message}</li>`);
+      });
+      res.write('</ul>');
+      res.write(
+        '<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>'
+      );
+      res.write('</body>');
+      res.write('</html>');
+      return res.end();
+    });
+  } else if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      // Append the new message to the file
+      fs.appendFile('messages.txt', message + '\n', err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
   } else {
-    title = '404 Not Found';
-    message = 'Page not found';
-    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.statusCode = 404;
+    res.end('Not Found');
   }
-
-  res.end(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>${title}</title>
-    </head>
-    <body>
-      <h1>${message}</h1>
-    </body>
-    </html>
-  `);
-  
 });
-
 
 const PORT = 4000;
 server.listen(PORT, () => {
