adb1168d03c86c1796b020e301e3cd7cbff1536f
Venkatareddykunduru <venkatareddykunduru123@gmail.com>
2024-07-04 10:53:04 +0530

    updated app.js file with get/post requests

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-// First middleware
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
+// Middleware to parse URL-encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route to show the form
+app.get('/add-product', (req, res) => {
+  res.send(`
+    <form action="/product" method="POST">
+      <label for="title">Product Title:</label>
+      <input type="text" id="title" name="title"><br><br>
+      <label for="size">Product Size:</label>
+      <input type="text" id="size" name="size"><br><br>
+      <button type="submit">Add Product</button>
+    </form>
+  `);
 });
 
-// Second middleware
-app.use((req, res, next) => {
-  console.log('Second middleware');
-  next();
-});
-
-// Route handler
-app.get('/', (req, res) => {
-  res.send('<h1> hello to node js </h1>');
+// Route to handle form submission
+app.post('/product', (req, res) => {
+  console.log(req.body); // Parsed form data
+  res.send('Product added!');
 });
 
 app.listen(3000, () => {
